99321c1b85f77d4c0b0ed2f6d877153ccd477f47
wolfsl <tharindupriyashan18@gmail.com>
2022-04-21 06:25:27 +0530

    main.js updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,3 @@
-
-
-console.log("Weather site is online!")
-
-
-
 const form=document.querySelector('form')
 const input=document.querySelector('input')
 const msg1=document.querySelector('#message-1')
